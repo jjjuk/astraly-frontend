5d5cf50bff4b6aaa51b907004f1ca70edab03d5e
Evolve <Artevolve@yahoo.com>
2022-05-07 12:23:07 +0200

    feat: lottery_token hooks 🔨

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -6,10 +6,12 @@ export const NETWORK_LABEL = {
 export const Contracts = {
   SN_MAIN: {
     token: '',
-    staking: ''
+    staking: '',
+    lottery_token: ''
   },
   SN_GOERLI: {
     token: '0x07a6dde277913b4e30163974bf3d8ed263abb7c7700a18524f5edf38a13d39ec',
-    staking: '0x057a1cd8df302a198dc4a673aef525b9374cfeb9f161192efb54f684b27563ea'
+    staking: '0x057a1cd8df302a198dc4a673aef525b9374cfeb9f161192efb54f684b27563ea',
+    lottery_token: '0x005d381d20541d86b57a33433d40f066191930b986507bf7c20e918f9a11de1a'
   }
 };
