9b660fb1c9d6f3923b866a841e0e503dd1f9c320
Matteo Georges <matteo.georges74@hotmail.fr>
2022-05-13 10:47:00 +0200

    feat: add project page

--- src/pages/project/[pid].tsx
+++ src/pages/project/[pid].tsx
@@ -5,7 +5,7 @@ import {Project, Round} from 'interfaces';
 import {projects} from 'utils/data';
 import {
   Badge,
-  Box,
+  Button,
   Flex,
   Heading,
   Image,
@@ -14,7 +14,11 @@ import {
   Stack,
   Tag,
   Text,
-  UnorderedList
+  UnorderedList,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator
 } from '@chakra-ui/react';
 
 const ProjectPage = () => {
@@ -28,6 +32,301 @@ const ProjectPage = () => {
 
   return (
     <Layout>
+      <Breadcrumb color={'#9D69DE'}>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/launchpad" color={'#9D69DE'}>
+            Launchpad
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" color={'#8F00FF'} fontWeight="900">
+            {project?.name}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Flex className="ProjectTitleWrapper" gridGap={'20px'} mt="25px">
+        <a href="javascript:history.back()">
+          <Image src="/assets/imgs/arrow.png" height={'43px'} />
+        </a>
+        <Heading
+          size="2xl"
+          color="purple.700"
+          textShadow="-2px 2px 0px #8f00ff"
+          mb={'64px'}
+          textTransform="uppercase"
+        >
+          {project?.name}
+        </Heading>
+      </Flex>
+      <Flex className="Container Project" flexDir={'row'}>
+        <Flex className="Left Container" width={'70%'} flexDir="column">
+          <Flex
+            className="Steps Container"
+            width={'100%'}
+            borderRadius="24px"
+            border={'2px solid #fff'}
+            bg="#FAF3FF"
+          >
+            <Flex
+              className="Step 1"
+              width={'25%'}
+              borderRadius="24px"
+              padding="20px"
+              flexDir={'column'}
+              height={'100%'}
+            >
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                <Image src="/assets/imgs/star.png" mr={'3px'} height="10px" mt="auto" mb="auto" />{' '}
+                Step 1
+              </Text>
+              <Text
+                fontFamily="Druk Wide Web"
+                fontWeight="700"
+                fontSize="12px"
+                lineHeight="150%"
+                color="#8F00FF"
+                mt="5px"
+              >
+                Ticket Claim
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                You can claim your tickets.
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                2022-06-06
+              </Text>
+              <Button
+                bg="linear-gradient(360deg, #7E1AFF 0%, #9F24FF 50%)"
+                borderRadius="8px"
+                boxShadow="0px 20px 35px rgba(55, 0, 99, 0.2)"
+                width="100%"
+                fontFamily="Druk Wide Web"
+                color="white"
+                transition="all 0.5s ease"
+                _hover={{bg: 'linear-gradient(360deg, #9F24FF 0%,#7E1AFF  50%)'}}
+                mt="auto"
+                height={'24px'}
+              >
+                Claim
+              </Button>
+            </Flex>
+            <Flex
+              className="Step 2"
+              width={'25%'}
+              borderRadius="24px"
+              padding="20px"
+              flexDir={'column'}
+              height={'100%'}
+            >
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                <Image src="/assets/imgs/star.png" mr={'3px'} height="10px" mt="auto" mb="auto" />{' '}
+                Step 1
+              </Text>
+              <Text
+                fontFamily="Druk Wide Web"
+                fontWeight="700"
+                fontSize="12px"
+                lineHeight="150%"
+                color="#8F00FF"
+                mt="5px"
+              >
+                Ticket Claim
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                You can claim your tickets.
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                2022-06-06
+              </Text>
+              <Button
+                bg="linear-gradient(360deg, #7E1AFF 0%, #9F24FF 50%)"
+                borderRadius="8px"
+                boxShadow="0px 20px 35px rgba(55, 0, 99, 0.2)"
+                width="100%"
+                fontFamily="Druk Wide Web"
+                color="white"
+                transition="all 0.5s ease"
+                _hover={{bg: 'linear-gradient(360deg, #9F24FF 0%,#7E1AFF  50%)'}}
+                mt="auto"
+                height={'24px'}
+              >
+                Claim
+              </Button>
+            </Flex>
+            <Flex
+              className="Step 3"
+              width={'25%'}
+              borderRadius="24px"
+              padding="20px"
+              flexDir={'column'}
+              height={'100%'}
+            >
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                <Image src="/assets/imgs/star.png" mr={'3px'} height="10px" mt="auto" mb="auto" />{' '}
+                Step 1
+              </Text>
+              <Text
+                fontFamily="Druk Wide Web"
+                fontWeight="700"
+                fontSize="12px"
+                lineHeight="150%"
+                color="#8F00FF"
+                mt="5px"
+              >
+                Ticket Claim
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                You can claim your tickets.
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                2022-06-06
+              </Text>
+              <Button
+                bg="linear-gradient(360deg, #7E1AFF 0%, #9F24FF 50%)"
+                borderRadius="8px"
+                boxShadow="0px 20px 35px rgba(55, 0, 99, 0.2)"
+                width="100%"
+                fontFamily="Druk Wide Web"
+                color="white"
+                transition="all 0.5s ease"
+                _hover={{bg: 'linear-gradient(360deg, #9F24FF 0%,#7E1AFF  50%)'}}
+                mt="auto"
+                height={'24px'}
+              >
+                Claim
+              </Button>
+            </Flex>
+            <Flex
+              className="Step 4"
+              width={'25%'}
+              borderRadius="24px"
+              padding="20px"
+              flexDir={'column'}
+              height={'100%'}
+            >
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                <Image src="/assets/imgs/star.png" mr={'3px'} height="10px" mt="auto" mb="auto" />{' '}
+                Step 1
+              </Text>
+              <Text
+                fontFamily="Druk Wide Web"
+                fontWeight="700"
+                fontSize="12px"
+                lineHeight="150%"
+                color="#8F00FF"
+                mt="5px"
+              >
+                Ticket Claim
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                You can claim your tickets.
+              </Text>
+              <Text
+                fontWeight="400"
+                fontSize="16px"
+                lineHeight="22px"
+                color="#8F00FF"
+                display={'flex'}
+                flexDir="row"
+              >
+                2022-06-06
+              </Text>
+              <Button
+                bg="linear-gradient(360deg, #7E1AFF 0%, #9F24FF 50%)"
+                borderRadius="8px"
+                boxShadow="0px 20px 35px rgba(55, 0, 99, 0.2)"
+                width="100%"
+                fontFamily="Druk Wide Web"
+                color="white"
+                transition="all 0.5s ease"
+                _hover={{bg: 'linear-gradient(360deg, #9F24FF 0%,#7E1AFF  50%)'}}
+                mt="auto"
+                height={'24px'}
+              >
+                Claim
+              </Button>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex className="Right Container" width={'30%'} flexDir="column"></Flex>
+      </Flex>
       <Flex gap="20px" mt={2}>
         <Flex bg="#8f00ff" width="40%" p={7} flexDir="column" gap="10px">
           <Flex gap="20px" alignItems="center">
