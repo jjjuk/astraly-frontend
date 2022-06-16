a742346516ac957e22b77c483461b6d7b913d09e
Evolve <Artevolve@yahoo.com>
2022-06-16 19:41:43 +0200

    fix: faucet address ✨

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -16,7 +16,7 @@ export const Contracts = {
     token: '0x05a6b68181bb48501a7a447a3f99936827e41d77114728960f22892f02e24928',
     staking: '0x005ef67d8c38b82ba699f206bf0db59f1828087a710bad48cc4d51a2b0da4c29',
     lottery_token: '0x0552b530aacef8e7338c9a8f0fd4e9ad06100a67befc2f4e9246219a4e41da04',
-    faucet: '',
+    faucet: '0x02810b322f1709382244cebec85e47098d2b913e910ae5d3650aaa46ba6526fe',
     lp_token: '',
     eth: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
   },
