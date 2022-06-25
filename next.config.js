1e2588b9ea6c634416c2b1f2f7a77c64218d578b
Evolve <Artevolve@yahoo.com>
2022-06-25 14:59:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,19 +2,22 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: 'https://zkpad-api.herokuapp.com/api/graphql',
+  },
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
   async rewrites() {
     return [
       {
         source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js'
+        destination: 'https://cdn.splitbee.io/sb.js',
       },
       {
         source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug'
-      }
+        destination: 'https://hive.splitbee.io/:slug',
+      },
     ]
   },
   webpack: function (config) {
@@ -22,8 +25,8 @@ const nextConfig = {
       loader: 'file-loader',
       options: {
         outputPath: '../public/assets/',
-        publicPath: '/assets/'
-      }
+        publicPath: '/assets/',
+      },
     }
 
     config.module.rules.push({
@@ -31,15 +34,15 @@ const nextConfig = {
       oneOf: [
         {
           resourceQuery: /inline/,
-          loader: '@svgr/webpack'
+          loader: '@svgr/webpack',
         },
         {
-          ...fileLoader
-        }
-      ]
+          ...fileLoader,
+        },
+      ],
     })
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
